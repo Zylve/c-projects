47bc2684a1e3b6cdfdaceeaa0327ff7045795a7e
Zylve <70604613+Zylve@users.noreply.github.com>
2022-12-08 22:16:01 -0800

    Delete cmaketest directory

--- cmaketest/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(int argc, char** argv) {
-    std::cout << "Hello, World! (Called from: " << argv[0] << " with " << argc << " argument(s))" << std::endl;
-    return 0;
-}
